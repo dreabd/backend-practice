c40edfbe9f62b60583e328fc5a7ec96be6a0c91b
Andre Chris Abad <andreabad17@gmail.com>
2024-05-11 14:48:36 -0700

    moving to other computer

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,16 @@
 from flask import Flask
+from flask_migrate import Migrate
+from .config import Config
+
+from .models import db
+
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
 	return "Hello World"
+
+app.config.from_object(Config)
+
+db.init_app(app)
+Migrate(app, db)
