af53a16afd07f5dc75121239f808b57f734c9841
Andre Abad <andreabad17@gmail.com>
2024-05-11 15:10:51 -0700

    making file path mroe dynamic

--- app/csvSQLPYthon.py
+++ app/csvSQLPYthon.py
@@ -1,14 +1,25 @@
 import csv
 import pandas as pandas
 import sqlite3
+import os
+import pathlib
+
+
+csv_file = 'data.csv'
+
+filepath = f"{os.path.dirname(__file__)}/{csv_file}"
+ 
+print(filepath)
 
 df = pandas.read_csv(
-    filepath_or_buffer = "/Users/andreabad/Desktop/Projects/backend-practice/app/data.csv",
+    filepath_or_buffer = filepath,
     header = 0
 )
 
 print(df)
 
+db_location = f"{os.getcwd()}/db/data.db"
+print(db_location)
 connection = sqlite3.connect("/Users/andreabad/Desktop/Projects/backend-practice/db/data.db")
 
 df.to_sql(
@@ -16,5 +27,4 @@ df.to_sql(
     con = connection, 
     if_exists = "replace",
     index = False,
-
 )
